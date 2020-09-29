649100bdabf88b669cdc7916ad139c2e1e233abc
Danil <danil.lunyow@mail.ru>
2020-09-30 02:37:19 +0500

    test config file

--- src/main/java/com/urfu/Devy/Main.java
+++ src/main/java/com/urfu/Devy/Main.java
@@ -4,24 +4,37 @@ import main.java.com.urfu.Devy.Bot.Discord.DiscordBot;
 import main.java.com.urfu.Devy.Command.CommandsController;
 import main.java.com.urfu.Devy.Goups.GroupInfo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
-
+    public static String PATH_TO_PROPERTIES = "src/config.properties";
     public static void main(String[] args) {
-        var bot = new DiscordBot("nope");
-        CommandsController.constructCommandsDictionary();
-        var exampleGroup = new GroupInfo(1);
-        bot.addGroup(exampleGroup);
-        bot.receive(exampleGroup, "$ping");
+        try {
+            var fileInputSystem = new FileInputStream(PATH_TO_PROPERTIES);
+            var prop = new Properties();
+            prop.load(fileInputSystem);
+            var token = prop.getProperty("token", "wrong key");
 
-        bot.receive(exampleGroup, "$help");
-        Scanner in = new Scanner(System.in);
-        while(true){
-            var command = in.nextLine();
-            if(command == null || command.isEmpty())
-                continue;
-            bot.receive(exampleGroup, command);
+            var bot = new DiscordBot(token);
+            CommandsController.constructCommandsDictionary();
+            var exampleGroup = new GroupInfo(1);
+            bot.addGroup(exampleGroup);
+            bot.receive(exampleGroup, "$ping");
+
+            bot.receive(exampleGroup, "$help");
+            Scanner in = new Scanner(System.in);
+            while(true){
+                var command = in.nextLine();
+                if(command == null || command.isEmpty())
+                    continue;
+                bot.receive(exampleGroup, command);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
